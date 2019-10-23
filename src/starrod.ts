b501b59c1321be5d40f38e0919c605c8f4913cf3
alex <16batesa@gmail.com>
2019-10-23 04:57:43 +0100

    feat: 'Call' autocompletion

--- src/starrod.ts
+++ src/starrod.ts
@@ -3,6 +3,10 @@ import {
     ExtensionContext,
     Hover,
     MarkdownString,
+    SignatureHelp,
+    SignatureInformation,
+    ParameterInformation,
+    CompletionItem,
 } from 'vscode'
 
 import * as lib from './lib.json'
@@ -29,8 +33,6 @@ ${(args || []).map(({ name, type }: ArgDoc, i: number) =>
 }
 
 export async function activate(ctx: ExtensionContext) {
-    // TODO: signatureHelpProvider
-
     languages.registerHoverProvider('starrod', {
         provideHover(document, position, token) {
             const range = document.getWordRangeAtPosition(position, /[^\s()]+/)
@@ -66,4 +68,63 @@ export async function activate(ctx: ExtensionContext) {
             return null
         },
     })
+
+    languages.registerSignatureHelpProvider('starrod', {
+        provideSignatureHelp(document, position, token, context) {
+            const range = document.getWordRangeAtPosition(position, /[^\s()]+/)
+            const word = document.getText(range)
+
+            const line = document.lineAt(position).text.trim().split(/[\s()]+/)
+            const selected = line.indexOf(word)
+
+            // FIXME: 'selected' is wrong when multiple line words are the same
+
+            if (line[0] == 'Call') {
+                const doc = lib.functions
+                    .find((o: { name: string }) => o.name == line[1])
+
+                if (!doc) {
+                    return null
+                }
+
+                // Convert doc to a signature string
+                const sigStr = doc.name
+                    + '('
+                    + (doc.args || [])
+                        .map(({ name, type }) => `${name}: ${type}`)
+                        .join(' ')
+                    + ')'
+
+                const signature = new SignatureInformation(sigStr)
+                signature.parameters = (doc.args || []).map(({ name, type }) =>
+                    new ParameterInformation(`${name}: ${type}`)
+                )
+
+                const help = new SignatureHelp()
+                help.activeParameter = selected - 2
+                help.activeSignature = 0
+                help.signatures = [
+                    signature
+                ]
+
+                return help
+            }
+
+            return null
+        }
+    }, '(')
+
+    languages.registerCompletionItemProvider('starrod', {
+        provideCompletionItems(document, position, token, context) {
+            const line = document.lineAt(position).text.trim().split(/[\s()]+/)
+
+            if (line[0] !== 'Call' || !!line[2]) {
+                return null
+            }
+
+            return lib.functions
+                //.filter(o => o.name.startsWith(line[1]))
+                .map(o => new CompletionItem(o.name))
+        }
+    }, ' ')
 }
